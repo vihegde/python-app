198e9dca8fefef05c073dfcfbe31d8a661b435bb
vihegde <vinay.hegde123@gmail.com>
2026-05-06 23:57:23 +0530

    first commit adds pythonapp application with kubernetes

---
+++ src/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'message':'Hello World',
+        'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+        'hostname':socket.gethostname()
+    })
+
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify ({
+        'status':'UP'
+    })
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+
+
+
+
+
+#'
