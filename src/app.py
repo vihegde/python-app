8bc497e49c602334c084066bef9ebcc6fb7a1330
vihegde <vinay.hegde123@gmail.com>
2026-05-10 20:30:07 +0530

    adds ci github workflow

--- src/app.py
+++ src/app.py
@@ -9,7 +9,8 @@ def details():
     return jsonify({
         'message':'Hello World',
         'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
-        'hostname':socket.gethostname()
+        'hostname':socket.gethostname(),
+        'message':'You are doing g8t'
     })
 
 
